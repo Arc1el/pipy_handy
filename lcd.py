7734389af5efd5ef7b3e2085dbed32d9f5802694
Arc1el <khm970514@gmail.com>
2021-05-29 18:52:03 +0900

    Update lcd.py

--- lcd.py
+++ lcd.py
@@ -1,5 +1,6 @@
 import board
 import digitalio
+import time
 lcd_rs = digitalio.DigitalInOut(board.D26)
 lcd_en = digitalio.DigitalInOut(board.D19)
 lcd_d7 = digitalio.DigitalInOut(board.D22)
@@ -13,3 +14,4 @@ lcd_rows = 2
 import adafruit_character_lcd.character_lcd as characterlcd
 lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
 lcd.message = "Hello\nCircuitPython!"
+time.sleep(5)
