03a09055ca47e400b28aebbfe1c049c3de7df220
Arc1el <khm970514@gmail.com>
2021-06-01 20:37:21 +0900

    Update btn_interrupt.py

--- btn_interrupt.py
+++ btn_interrupt.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(12, GPIO.IN)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def my_callback(channel):
     print("btn pressed")
