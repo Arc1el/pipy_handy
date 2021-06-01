298713ce52ec159b9afc14634bb78b119ac9edf4
Arc1el <khm970514@gmail.com>
2021-06-01 20:30:39 +0900

    Create btn_interrupt.py

---
+++ btn_interrupt.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(12, GPIO.IN)
+
+def my_callback(channel):
+    print("btn pressed")
+    
+GPIO.add_event_detect(12, GPIO.FALLING, callback=my_callback)
+
+try:
+    while True:
+        time.sleep(30)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    
+GPIO.cleanup()
